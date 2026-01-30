0b095db2f700d2ff73206d57744e994fed697fac
muhammadshahrukh731 <muhammadshahrukh731@rocketmail.com>
2026-01-30 11:24:31 +0500

    backend curd

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+// CREATE
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// READ ALL
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// READ ONE
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
+});
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(updatedUser);
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+});
+
+module.exports = router;
